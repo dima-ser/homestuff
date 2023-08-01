1f1a09d74c9fef6ace10aaef18604d95dc35f00b
Dima <horseandcoon@mail.ru>
2023-07-31 17:23:07 -0700

    Add item model

---
+++ HomeStuff/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeStuff.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+    }
+}
