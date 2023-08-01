8cda251928bfd4cf7618611310bef7d15f38b398
Dima <horseandcoon@mail.ru>
2023-08-01 12:48:13 -0700

    CRUD for locations

---
+++ HomeStuff/Pages/LocationDelete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HomeStuff.Data;
+using HomeStuff.Models;
+
+namespace HomeStuff.Pages
+{
+    public class LocationDeleteModel : PageModel
+    {
+        private readonly HomeStuff.Data.SqliteContext _context;
+
+        public LocationDeleteModel(HomeStuff.Data.SqliteContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Location Location { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Location == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Location.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Location = location;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Location == null)
+            {
+                return NotFound();
+            }
+            var location = await _context.Location.FindAsync(id);
+
+            if (location != null)
+            {
+                Location = location;
+                _context.Location.Remove(Location);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Locations");
+        }
+    }
+}
