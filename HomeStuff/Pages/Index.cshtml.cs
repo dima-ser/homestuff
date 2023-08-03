993cd4e847a1d8a9ac2c536ade920b3435839222
Dima <horseandcoon@mail.ru>
2023-08-03 14:39:12 -0700

    Implement search

--- HomeStuff/Pages/Index.cshtml.cs
+++ HomeStuff/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using HomeStuff.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HomeStuff.Pages
 {
@@ -10,11 +11,17 @@ namespace HomeStuff.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly HomeStuff.Data.SqliteContext _context;
         public IList<HomeStuff.Models.Item> Items { get; set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? q { get; set; }
+        public SelectList? Locations { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? l { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger, HomeStuff.Data.SqliteContext context)
         {
             _logger = logger;
             _context = context;
+            Locations = new SelectList(context.Location, nameof(Location.Id), nameof(Location.Name));
         }
 
 
@@ -22,12 +29,33 @@ namespace HomeStuff.Pages
         {
             if (_context.Item != null)
             {
-                Items =  _context.Item.OrderByDescending(i => i.LastModifiedUtc).ToList();
+                var items = from i in _context.Item select i;
+                if (!string.IsNullOrEmpty(q))
+                {
+                    items = items.Where(s => s.Name.ToLower().Contains(q.ToLower()) || 
+                    (s.Description != null && s.Description.ToLower().Contains(q.ToLower())) ||
+                    (s.Manufacturer != null && s.Manufacturer.ToLower().Contains(q.ToLower())) ||
+                    (s.ModelNumber != null && s.ModelNumber.ToLower().Contains(q.ToLower())) ||
+                    (s.SerialNumber != null && s.SerialNumber.ToLower().Contains(q.ToLower())) ||
+                    (s.Vendor != null && s.Vendor.ToLower().Contains(q.ToLower())) ||
+                    (s.SKU != null && s.SKU.ToLower().Contains(q.ToLower())));
+                }
+                if (!string.IsNullOrEmpty(l))
+                {
+                    items = items.Where(i => i.LocationId.ToString() == l);
+                }
+
+                Items = items.OrderByDescending(i => i.LastModifiedUtc).ToList();
                 foreach (var item in Items)
                 {
                     item.Location = _context.Location.FirstOrDefault(l => l.Id == item.LocationId);
                 }
             }
+        }
+
+        public void OnPostSearch()
+        {
+
         }
     }
 }
