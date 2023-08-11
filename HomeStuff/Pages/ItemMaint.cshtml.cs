5dadacb751c26791952a3466d102e8029cda375e
Dima <horseandcoon@mail.ru>
2023-08-11 08:21:12 -0700

    Tweaks to maintenance log

--- HomeStuff/Pages/ItemMaint.cshtml.cs
+++ HomeStuff/Pages/ItemMaint.cshtml.cs
@@ -15,20 +15,18 @@ namespace HomeStuff.Pages
         private readonly SqliteContext _context;
         [BindProperty(SupportsGet = true)]
         public int ItemId { get; set; }
-        //public List<Models.Maintenance> Maintenances { get; set; } = new List<Models.Maintenance>();
-        public List<Models.Maintenance> OverdueMaintenances { get; set; } = new List<Models.Maintenance>();
-        public List<Models.Maintenance> UpcomingMaintenances { get; set; } = new List<Models.Maintenance>();
+        public List<Models.Maintenance> ScheduledMaintenances { get; set; } = new List<Models.Maintenance>();
         public List<Models.Maintenance> CompletedMaintenances { get; set; } = new List<Models.Maintenance>();
         public Item? Item;
 
         [BindProperty]
         public Models.Maintenance NewMaintenance { get; set; } = new Models.Maintenance();
         [BindProperty, DisplayName("Recurrence")]
-        public bool NewMaintRecurring { get; set; } = false;
-        [BindProperty, DisplayName("Every (days)")]
-        public int? NewMaintRecurringDays { get; set; } = 180;
-        [BindProperty, DisplayName("# of Occurences")]
-        public int? NewMaintNumOccurences { get; set; } = 10;
+        public bool NewMaintIsRecurring { get; set; } = false;
+        [BindProperty, DisplayName("Every # days")]
+        public int? NewMaintRecurrenceFrequency { get; set; } = 180;
+        [BindProperty, DisplayName("for # years")]
+        public int? NewMaintRecurrenceDuration { get; set; } = 2;
         [BindProperty(SupportsGet = true)]
         public string? ErrorMessage { get; set; } = string.Empty;
 
@@ -46,9 +44,8 @@ namespace HomeStuff.Pages
                 return NotFound();
             }
             DateOnly today = DateOnly.FromDateTime(DateTime.Now);
-            OverdueMaintenances = _context.Maintenance.Where(i => i.Item!.Id == ItemId && i.Date < today && i.Completed == false).ToList();
-            UpcomingMaintenances = _context.Maintenance.Where(i => i.Item!.Id == ItemId && i.Date >= today && i.Completed == false).ToList();
-            CompletedMaintenances = _context.Maintenance.Where(i => i.Item!.Id == ItemId && i.Completed == true).ToList();
+            ScheduledMaintenances = _context.Maintenance.OrderBy(i => i.Date).Where(i => i.Item!.Id == ItemId && i.Completed == false).ToList();
+            CompletedMaintenances = _context.Maintenance.OrderByDescending(i => i.Date).Where(i => i.Item!.Id == ItemId && i.Completed == true).ToList();
             NewMaintenance.ItemId = Item.Id;
             ViewData["Title"] = Item.Name;
             return Page();
@@ -66,28 +63,33 @@ namespace HomeStuff.Pages
             {
                 return Page();
             }
-            if (!NewMaintenance.Completed && NewMaintRecurring) 
+            if (!NewMaintenance.Completed && NewMaintIsRecurring) 
             {
-                if (NewMaintRecurringDays == null || NewMaintNumOccurences == null)
+                if (NewMaintRecurrenceFrequency == null || NewMaintRecurrenceDuration == null)
                 {
-                    return RedirectToPage("./ItemMaint", new { itemid = ItemId.ToString() , errormessage = "Please provide recurrence days and number of occurences" });
+                    return RedirectToPage("./ItemMaint", new { itemid = ItemId.ToString() , errormessage = "Please provide recurrence frequency and duration" });
                 }
                 else
                 {
                     //List<Models.Maintenance> schedule = new List<Models.Maintenance>((int)NewMaintNumOccurences);
                     //Models.Maintenance maintenance = NewMaintenance;
                     //schedule.Add(maintenance);
-                    for (int i = 0; i < NewMaintNumOccurences; i++)
+                    DateOnly maxDate = NewMaintenance.Date.AddYears((int)NewMaintRecurrenceDuration);
+                    DateOnly occurenceDate = NewMaintenance.Date;
+                    int i = 0;
+                    do
                     {
+                        occurenceDate = occurenceDate.AddDays(i * (int)NewMaintRecurrenceFrequency);
                         Models.Maintenance maintenance = new()
                         {
                             ItemId = NewMaintenance.ItemId,
-                            Date = NewMaintenance.Date.AddDays(i * (int)NewMaintRecurringDays),
+                            Date = occurenceDate,
                             Description = NewMaintenance.Description,
                             Completed = NewMaintenance.Completed
                         };
                         _context.Maintenance.Add(maintenance);
-                    }
+                        i++;
+                    } while (occurenceDate <= maxDate);
                 }
             }
             else
