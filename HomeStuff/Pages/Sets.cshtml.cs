b1b917b353df77e3c06da9dfa0c6a027fc32f11d
Dima <horseandcoon@mail.ru>
2024-03-20 14:29:40 -0700

    Add sets index page

---
+++ HomeStuff/Pages/Sets.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HomeStuff.Data;
+using HomeStuff.Models;
+
+namespace HomeStuff.Pages
+{
+    public class SetsModel : PageModel
+    {
+        private readonly HomeStuff.Data.SqliteContext _context;
+
+        public SetsModel(HomeStuff.Data.SqliteContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ItemSet> ItemSet { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.ItemSet != null)
+            {
+                ItemSet = await _context.ItemSet.ToListAsync();
+            }
+        }
+    }
+}
