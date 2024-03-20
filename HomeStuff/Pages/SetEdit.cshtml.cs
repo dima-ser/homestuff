b4fe3bf87f833534121063d3fb3bd40ef4101caf
Dima <horseandcoon@mail.ru>
2024-03-20 14:48:41 -0700

    Set new and edit pages

---
+++ HomeStuff/Pages/SetEdit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HomeStuff.Data;
+using HomeStuff.Models;
+
+namespace HomeStuff.Pages
+{
+    public class SetEditModel : PageModel
+    {
+        private readonly HomeStuff.Data.SqliteContext _context;
+
+        public SetEditModel(HomeStuff.Data.SqliteContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ItemSet ItemSet { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.ItemSet == null)
+            {
+                return NotFound();
+            }
+
+            var itemSet =  await _context.ItemSet.FirstOrDefaultAsync(m => m.Id == id);
+            if (itemSet == null)
+            {
+                return NotFound();
+            }
+            ItemSet = itemSet;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(ItemSet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemSetExists(ItemSet.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Sets");
+        }
+
+        private bool ItemSetExists(int id)
+        {
+          return (_context.ItemSet?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ HomeStuff/Pages/SetNew.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HomeStuff.Data;
+using HomeStuff.Models;
+
+namespace HomeStuff.Pages
+{
+    public class SetNewModel : PageModel
+    {
+        private readonly HomeStuff.Data.SqliteContext _context;
+        [BindProperty(SupportsGet = true)]
+        public string? RedirectUrl { get; set; } = null;
+
+        public SetNewModel(HomeStuff.Data.SqliteContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public ItemSet ItemSet { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.ItemSet == null || ItemSet == null)
+            {
+                return Page();
+            }
+
+            _context.ItemSet.Add(ItemSet);
+            await _context.SaveChangesAsync();
+            if (string.IsNullOrEmpty(RedirectUrl))
+                return RedirectToPage("./Sets");
+            else
+                return Redirect(RedirectUrl);
+        }
+    }
+}
